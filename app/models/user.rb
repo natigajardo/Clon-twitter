0346ddcfbea04a49ee2096a83eea3010bf747879
Natalia <natty.gajardo.s@gmail.com>
2020-08-18 20:52:48 -0400

    segunda historia

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tweets    
+  has_many :likes
+  has_many :liked_tweets, :through => :likes, :source => :tweet 
+
 end
